e5b6b831c03adeda2d73719df84579faaf6ef83d
Alex Morega <alex@grep.ro>
2012-10-25 15:49:46 +0300

    extract assertion; check for country

--- bdr_registry/tests.py
+++ bdr_registry/tests.py
@@ -8,6 +8,10 @@ class FormSubmitTest(unittest.TestCase):
     def setUp(self):
         self.client = Client()
 
+    def assert_object_has_items(self, obj, data):
+        for key in data:
+            self.assertEqual(getattr(obj, key), data[key])
+
     def test_submitted_organisation_is_saved(self):
         org_text_data = {
             'name': "Teh company",
@@ -16,13 +20,16 @@ class FormSubmitTest(unittest.TestCase):
             'addr_postalcode': "123456",
             'addr_place2': "Hovedstaden",
         }
+        denmark = models.Country.objects.get(name="Denmark")
         resp = self.client.post('/organisation/add', dict(org_text_data, **{
-            'country': models.Country.objects.get(name="Denmark").pk,
+            'country': denmark.pk,
         }))
+
         self.assertEqual(models.Organisation.objects.count(), 1)
         org = models.Organisation.objects.all()[0]
-        for key in org_text_data:
-            self.assertEqual(getattr(org, key), org_text_data[key])
+        self.assert_object_has_items(org, org_text_data)
+        self.assertEqual(org.country, denmark)
+
         self.assertEqual(resp.status_code, 302)
         self.assertEqual(resp['location'],
                          'http://testserver/organisation/%d' % org.pk)
