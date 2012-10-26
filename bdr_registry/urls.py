86daa590c36d88b7142bf66e3b9c2e01a5b58b80
Alex Morega <alex@grep.ro>
2012-10-26 19:03:54 +0300

    homepage redirects to self_register

--- bdr_registry/urls.py
+++ bdr_registry/urls.py
@@ -1,16 +1,20 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
+from django.views.generic.simple import redirect_to
+from django.core.urlresolvers import reverse_lazy
 import views
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+     url(r'^$', redirect_to, {'url': reverse_lazy('self_register')}),
      url(r'^admin/', include(admin.site.urls)),
      url(r'^organisation/add$', views.OrganisationCreate.as_view()),
      url(r'^organisation/(?P<pk>\d+)$', views.Organisation.as_view(),
             name='organisation'),
-     url(r'^self_register$', views.SelfRegister.as_view()),
+     url(r'^self_register$', views.SelfRegister.as_view(),
+            name='self_register'),
      url(r'^self_register/done$',
             TemplateView.as_view(template_name='self_register_done.html'),
             name='self_register_done'),
