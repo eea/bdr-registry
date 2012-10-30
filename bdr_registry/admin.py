5997d52a7d09ffe4d70f2b8e65a3728db4980182
Alex Morega <alex@grep.ro>
2012-10-30 18:05:12 +0200

    admin - organisation listing

--- bdr_registry/admin.py
+++ bdr_registry/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 import models
 
 
+class OrganisationAdmin(admin.ModelAdmin):
+
+    list_filter = ['country']
+    list_display = ['__unicode__', 'account']
+
+
 admin.site.register(models.Country)
-admin.site.register(models.Organisation)
+admin.site.register(models.Organisation, OrganisationAdmin)
 admin.site.register(models.Person)
 admin.site.register(models.Obligation)
 admin.site.register(models.ApiKey)
