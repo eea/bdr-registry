f9215a0ee8254401a26e9397913024e4d3040eb7
Alex Morega <alex@grep.ro>
2012-12-07 15:40:16 +0200

    script to import organisation accounts from ldap

--- bdr_registry/scripts.py
+++ bdr_registry/scripts.py
@@ -1,8 +1,14 @@
+import logging
 import csv
 from datetime import datetime, timedelta
 from django.utils import timezone
 from django.db import transaction
+from django.conf import settings
 from . import models
+
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 
 
 HOURS_2 = timedelta(hours=2)
@@ -52,3 +58,44 @@ def import_companies(csv_file):
         p1 = models.Person.objects.create(organisation=org, **p1_data)
         if any(p2_data.values()):
             p2 = models.Person.objects.create(organisation=org, **p2_data)
+
+
+@transaction.commit_on_success
+def update_companies_from_ldap():
+    import ldap
+    conn = ldap.initialize(settings.AUTH_LDAP_SERVER_URI)
+    res = conn.search_s("ou=Business Reporters,o=Eionet,l=Europe",
+                        ldap.SCOPE_SUBTREE,
+                        '(objectClass=organizationalRole)')
+    for dn, attrs in res:
+        [uid] = attrs['uid']
+        [cn] = attrs['cn']
+        org_name, country_name = [v.strip().decode('utf-8')
+                                  for v in cn.rsplit('/', 1)]
+        account, is_new = models.Account.objects.get_or_create(uid=uid)
+        if not is_new:
+            log.debug("Account uid=%r already in db, skipping.", uid)
+            continue
+
+        country = models.Country.objects.get(name=country_name.title())
+
+        if uid.startswith('fgas'):
+            obligation = models.Obligation.objects.get(code='fgas')
+        elif uid.startswith('ods'):
+            obligation = models.Obligation.objects.get(code='ods')
+        else:
+            log.warn("Can't determine obligation for uid=%r", uid)
+            continue
+
+        org = models.Organisation.objects.create(
+            name=org_name,
+            addr_street="",
+            addr_place1="",
+            addr_postalcode="",
+            addr_place2="",
+            country=country,
+            obligation=obligation,
+            account=account)
+
+        log.info("Organisation: uid=%r pk=%r name=%r",
+                 org.account.uid, org.pk, org.name)
