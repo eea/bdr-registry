013e5a6963c94c98cdc1d0f34dfb55fb4885493d
Alex Morega <alex@grep.ro>
2013-01-21 20:16:33 +0200

    create ldap accounts and update their password

--- bdr_registry/ldap_editor.py
+++ bdr_registry/ldap_editor.py
@@ -1,7 +1,16 @@
 import ldap
+import hashlib
+import base64
+
+
+def encrypt_password(password):
+    return "{SHA}" + base64.b64encode(hashlib.sha1(password).digest())
 
 
 class LdapEditor(object):
+
+    encoding = 'utf-8'
+    users_dn = "ou=Business Reporters,o=EIONET,l=Europe"
 
     def __init__(self, server, bind_dn, bind_password):
         conn = ldap.initialize(server)
@@ -9,6 +18,36 @@ class LdapEditor(object):
         conn.timeout = 5
         result = conn.simple_bind_s(bind_dn, bind_password)
         self.conn = conn
+
+    def _account_dn(self, uid):
+        return 'uid=' + uid + ',' + self.users_dn
+
+    def create_account(self, uid, org_name, country_name):
+        name = "%s / %s" % (org_name, country_name)
+        attrs = [
+            ('uid', [uid.encode(self.encoding)]),
+            ('cn', [name.encode(self.encoding)]),
+            ('objectClass', ['top', 'organizationalRole',
+                             'simpleSecurityObject', 'uidObject']),
+            ('userPassword', [encrypt_password('')]),
+        ]
+
+        try:
+            result = self.conn.add_s(self._account_dn(uid), attrs)
+
+        except ldap.ALREADY_EXISTS:
+            return False
+
+        else:
+            assert result == (ldap.RES_ADD, [])
+            return True
+
+    def reset_password(self, uid, password):
+        attrs = [
+            (ldap.MOD_REPLACE, 'userPassword', [encrypt_password(password)]),
+        ]
+        result = self.conn.modify_s(self._account_dn(uid), attrs)
+        assert result == (ldap.RES_MODIFY, [])
 
 
 def create_ldap_editor():
