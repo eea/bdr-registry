b42a723663e5994a9e7901a332e3740b5882b02d
Dragos Catarahia <dragos@catarahia.me>
2014-03-21 16:27:45 +0200

    Organisations search fields

--- management/views/organisations.py
+++ management/views/organisations.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.db.models import Q
-from braces.views import LoginRequiredMixin, StaffuserRequiredMixin
+from braces.views import (LoginRequiredMixin, StaffuserRequiredMixin,
+                          AjaxResponseMixin)
 
 from bdr_registry.models import Organisation
 from management.base import FilterView
@@ -25,10 +26,18 @@ class OrganisationsFilter(LoginRequiredMixin,
             queryset = queryset.order_by(opt['order_by'])
 
         if 'search' in opt and opt['search']:
-            search_filters = Q(name__icontains=opt['search'])
+            search_filters = (
+                Q(name__icontains=opt['search']) |
+                Q(account__uid__icontains=opt['search']) |
+                Q(addr_postalcode__icontains=opt['search']) |
+                Q(vat_number__icontains=opt['search']) |
+                Q(eori__icontains=opt['search'])
+            )
             queryset = queryset.filter(search_filters)
 
         if opt['count']:
             return queryset.count()
 
         return queryset[opt['offset']: opt['limit']]
+
+
