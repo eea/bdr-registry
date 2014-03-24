ce11b91e58738d140cafd808d53b3085545b356d
Ștefan Mihăilă <stefan.mihaila@gmail.com>
2014-03-24 15:56:08 +0200

    Reset filter on '(Nothing)'

--- management/static/js/main.js
+++ management/static/js/main.js
@@ -3,9 +3,14 @@ $(function () {
     $('table[data-toggle=datatable]').customDataTable();
 
     $('.filter_data').on('change', function () {
-        var val = $(this).find('option:selected').text();
-        var index = $(this).data('column-index')
-        dt.fnFilter(val, index);
+        var selectedOption = $(this).find('option:selected');
+        var index = $(this).data('column-index');
+
+        if (selectedOption.val() > 0) {
+            dt.fnFilter(selectedOption.text(), index);
+        } else {
+            dt.fnFilter('', index);
+        }
     });
 
 });
