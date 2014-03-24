18082efbe010c1c6904b6b018f8a675a7147c9a4
Cornel Nițu <cornel.nitu@eaudeweb.ro>
2014-03-24 12:43:11 +0200

    CustomBooleanFieldListFilter added (refs #19033)

--- bdr_registry/admin.py
+++ bdr_registry/admin.py
@@ -263,6 +263,32 @@ class CommentReadOnlyInline(CommentInline):
     readonly_fields = ['text']
 
 
+class CustomBooleanFieldListFilter(admin.FieldListFilter):
+    def __init__(self, field, request, params, model, model_admin, field_path):
+        self.lookup_kwarg = '%s__isnull' % field_path
+        self.lookup_val = request.GET.get(self.lookup_kwarg, None)
+        super(CustomBooleanFieldListFilter, self).__init__(field,
+            request, params, model, model_admin, field_path)
+
+    def expected_parameters(self):
+        return [self.lookup_kwarg]
+
+    def choices(self, cl):
+        for lookup, title in (
+                (None, 'All'),
+                ('1', 'without account'),
+                ('0', 'with account')):
+            yield {
+                'selected': self.lookup_val == lookup,
+                'query_string': cl.get_query_string({
+                    self.lookup_kwarg: lookup,
+                }),
+                'display': title,
+            }
+
+admin.FieldListFilter.register(lambda f: isinstance(f,
+    (models.OneToOneField)), CustomBooleanFieldListFilter)
+
 
 class OrganisationAdmin(ReadOnlyAdmin):
 
@@ -272,7 +298,13 @@ class OrganisationAdmin(ReadOnlyAdmin):
                      'obligation', 'account', 'comments']
     user_readonly_inlines = [CommentReadOnlyInline, PersonReadOnlyInline]
 
-    list_filter = ['obligation', 'country']
+    list_filter = [
+        ('date_registered', admin.DateFieldListFilter),
+        ('account', CustomBooleanFieldListFilter),
+        'obligation',
+        'country'
+    ]
+
     list_display = ['id', 'name', 'obligation', 'account', 'country',
                     'addr_postalcode', 'vat_number', 'eori']
     list_display_links = ('name',)
