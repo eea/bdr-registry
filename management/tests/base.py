0934fce4c0124a7bf9ed641d3df37371350e3fe7
Dragos Catarahia <dragos@catarahia.me>
2014-03-28 17:55:25 +0200

    Added base webtest class

---
+++ management/tests/base.py
@@ -0,0 +1,66 @@
+
+from django.db.models import Model
+from django.core.urlresolvers import reverse
+from django.db.models.loading import get_model
+
+from django_webtest import WebTest
+from webtest import AppError
+from webtest.forms import Select, MultipleSelect
+
+
+class BaseWebTest(WebTest):
+
+    csrf_checks = False
+
+    def __init__(self, *args, **kwargs):
+        self.AppError = AppError
+        super(BaseWebTest, self).__init__(*args, **kwargs)
+
+    def populate_fields(self, form, data):
+        for field_name, field in form.field_order:
+            if field_name in data:
+                value = data[field_name]
+                if isinstance(value, Model):
+                    value = value.pk
+                if isinstance(field, MultipleSelect):
+                    if not isinstance(value, list):
+                        value = [value]
+                if isinstance(field, (Select, MultipleSelect)):
+                    field.force_value(value)
+                else:
+                    field.value = value
+        return form
+
+    def normalize_data(self, data):
+
+        def convert_model_to_pk(value):
+            if isinstance(value, Model):
+                return value.pk
+            return value
+
+        new_data = dict(data)
+        for k, v in new_data.items():
+            if isinstance(v, list):
+                new_data[k] = map(convert_model_to_pk, v)
+            else:
+                new_data[k] = convert_model_to_pk(v)
+        return new_data
+
+    def reverse(self, view_name, *args, **kwargs):
+        return reverse(view_name, args=args, kwargs=kwargs)
+
+    def assertObjectInDatabase(self, model, **kwargs):
+        app = kwargs.pop('app', 'live_catalogue')
+        if isinstance(model, basestring):
+            Model = get_model(app, model)
+        else:
+            Model = model
+
+        if not Model:
+            self.fail('Model {} does not exist'.format(model))
+        try:
+            return Model.objects.get(**kwargs)
+        except Model.DoesNotExist:
+            self.fail('Object "{}" with kwargs {} does not exist'.format(
+                model, str(kwargs)
+            ))
