13c7c9363344cc0e2fcc9d2bbebfd54a85e3564e
Ștefan Mihăilă <stefan.mihaila@gmail.com>
2014-04-04 13:19:07 +0300

    Person breadcrumbs

--- bdr_management/views/persons.py
+++ bdr_management/views/persons.py
@@ -1,24 +1,25 @@
-from django.contrib import messages
-from django.contrib.messages.views import SuccessMessageMixin
-from django.core.urlresolvers import reverse, reverse_lazy
-from django.db.models import Q
-from django.shortcuts import get_object_or_404
-from django.utils.translation import ugettext as _
 from django.views import generic
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
+from django.utils.translation import ugettext as _
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 
-from braces.views import StaffuserRequiredMixin, GroupRequiredMixin
+from braces import views
 
-from bdr_management import base, forms
 from bdr_registry.models import Person, Organisation
+from bdr_management import base
+from bdr_management.forms import PersonForm
 
 
-class Persons(StaffuserRequiredMixin,
+class Persons(views.StaffuserRequiredMixin,
               generic.TemplateView):
 
     template_name = 'bdr_management/persons.html'
 
 
-class PersonsFilter(StaffuserRequiredMixin,
+class PersonsFilter(views.StaffuserRequiredMixin,
                     base.FilterView):
 
     def process_name(self, obj, val):
@@ -56,32 +57,122 @@ class PersonsFilter(StaffuserRequiredMixin,
         return queryset[opt['offset']: opt['limit']]
 
 
-class PersonsView(StaffuserRequiredMixin,
-                  base.ModelTableViewMixin,
-                  generic.DetailView):
+class PersonsBaseView(base.ModelTableViewMixin,
+                      generic.DetailView):
 
     template_name = 'bdr_management/person_view.html'
     model = Person
     exclude = ('id', )
 
-    def get_edit_url(self):
-        return reverse('management:persons_edit',
-                       kwargs={'pk': self.kwargs['pk']})
-
     def get_delete_url(self):
-        return reverse('management:persons_delete',
-                       kwargs={'pk': self.kwargs['pk']})
+        return reverse('management:persons_delete', kwargs=self.kwargs)
 
 
-class PersonAdd(GroupRequiredMixin,
-                base.ModelTableEditMixin,
+class PersonsManagementView(views.StaffuserRequiredMixin,
+                            PersonsBaseView):
+
+    def get_context_data(self, **kwargs):
+        breadcrumbs = [
+            base.Breadcrumb(reverse('home'), title=_('Registry')),
+            base.Breadcrumb(reverse('management:persons'),
+                            _('Persons')),
+            base.Breadcrumb('', self.object)
+        ]
+        data = super(PersonsManagementView, self).get_context_data(**kwargs)
+        data['breadcrumbs'] = breadcrumbs
+        return data
+
+    def get_edit_url(self):
+        return reverse('management:persons_edit', kwargs=self.kwargs)
+
+
+class PersonsUpdateView(base.OrganisationUserRequiredMixin,
+                        PersonsBaseView):
+
+    def get_context_data(self, **kwargs):
+        breadcrumbs = [
+            base.Breadcrumb(reverse('home'), title=_('Registry')),
+            base.Breadcrumb('', self.object)
+        ]
+        data = super(PersonsUpdateView, self).get_context_data(**kwargs)
+        data['breadcrumbs'] = breadcrumbs
+        return data
+
+    def get_edit_url(self):
+        return reverse('person_update', kwargs=self.kwargs)
+
+
+class PersonBaseEdit(base.ModelTableViewMixin,
+                     SuccessMessageMixin,
+                     generic.UpdateView):
+
+    template_name = 'bdr_management/person_edit.html'
+    model = Person
+    success_message = _('Person edited successfully')
+
+
+class PersonsManagementEdit(views.GroupRequiredMixin,
+                            PersonBaseEdit):
+
+    group_required = 'BDR helpdesk'
+
+    def get_context_data(self, **kwargs):
+        breadcrumbs = [
+            base.Breadcrumb(reverse('home'), title=_('Registry')),
+            base.Breadcrumb(reverse('management:persons'),
+                            _('Persons')),
+            base.Breadcrumb(reverse('management:persons_view',
+                            kwargs={'pk': self.object.pk}),
+                            self.object),
+            base.Breadcrumb('', _('Edit %s' % self.object))
+        ]
+        data = super(PersonsManagementEdit, self).get_context_data(**kwargs)
+        data['breadcrumbs'] = breadcrumbs
+        return data
+
+    def get_success_url(self):
+        return reverse('management:persons_view', kwargs=self.kwargs)
+
+
+class PersonsUpdate(base.OrganisationUserRequiredMixin,
+                    PersonsBaseView):
+
+    def get_context_data(self, **kwargs):
+        breadcrumbs = [
+            base.Breadcrumb(reverse('home'), _('Registry')),
+            base.Breadcrumb(reverse('person', kwargs={'pk': self.object.pk}),
+                            self.object),
+            base.Breadcrumb('', _('Edit %s' % self.object))
+        ]
+        data = super(PersonsUpdate, self).get_context_data(**kwargs)
+        data['breadcrumbs'] = breadcrumbs
+        return data
+
+    def get_success_url(self):
+        return reverse('person', kwargs=self.kwargs)
+
+
+class PersonDelete(views.GroupRequiredMixin,
+                   base.ModelTableEditMixin,
+                   generic.DeleteView):
+
+    group_required = 'BDR helpdesk'
+    model = Person
+    success_url = reverse_lazy('management:persons')
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, _("Person deleted"))
+        return super(PersonDelete, self).delete(request, *args, **kwargs)
+
+
+class PersonAdd(views.GroupRequiredMixin,
                 SuccessMessageMixin,
                 generic.CreateView):
 
     template_name = 'bdr_management/person_add.html'
     group_required = 'BDR helpdesk'
     model = Person
-    form_class = forms.PersonForm
+    form_class = PersonForm
     success_message = _('Person created successfully')
 
     def dispatch(self, *args, **kwargs):
@@ -101,30 +192,3 @@ class PersonAdd(GroupRequiredMixin,
         context['title'] = 'Add a new person'
         context['object'] = self.organisation
         return context
-
-
-class PersonEdit(GroupRequiredMixin,
-                 base.ModelTableEditMixin,
-                 SuccessMessageMixin,
-                 generic.UpdateView):
-
-    template_name = 'bdr_management/person_edit.html'
-    group_required = 'BDR helpdesk'
-    model = Person
-    success_message = _('Person edited successfully')
-
-    def get_success_url(self):
-        return reverse('management:persons_view', kwargs=self.kwargs)
-
-
-class PersonDelete(GroupRequiredMixin,
-                   base.ModelTableEditMixin,
-                   generic.DeleteView):
-
-    group_required = 'BDR helpdesk'
-    model = Person
-    success_url = reverse_lazy('management:persons')
-
-    def delete(self, request, *args, **kwargs):
-        messages.success(request, _("Person deleted"))
-        return super(PersonDelete, self).delete(request, *args, **kwargs)
