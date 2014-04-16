52489083ac3af4c238268f3a3f746b90bdd1e674
Ștefan Daniel Mihăilă <stefan.mihaila@gmail.com>
2014-04-16 18:39:31 +0300

    Refactor tests

--- bdr_management/tests/test_organisations.py
+++ bdr_management/tests/test_organisations.py
@@ -29,17 +29,17 @@ class OrganisationResetPasswordTests(base.BaseWebTest):
         url = self.reverse('management:reset_password', pk=org.pk)
         resp = self.app.get(url, user='admin')
         self.assertEqual(200, resp.status_int)
-        self.assertEqual('Reset password for %s' % org.name,
-                         resp.pyquery.find('h1').text())
+        self.assertTemplateUsed(resp, 'bdr_management/reset_password.html')
+        self.assertEqual(resp.context['object'], org)
 
-    def test_reset_passowrd_get_without_account(self):
+    def test_reset_password_get_without_account(self):
         user = factories.StaffUserFactory()
         org = factories.OrganisationFactory()
         url = self.reverse('management:reset_password', pk=org.pk)
         resp = self.app.get(url, user=user.username, expect_errors=True)
         self.assertEqual(404, resp.status_int)
 
-    def test_reset_passowrd_post_without_account(self):
+    def test_reset_password_post_without_account(self):
         user = factories.StaffUserFactory()
         org = factories.OrganisationFactory()
         url = self.reverse('management:reset_password', pk=org.pk)
@@ -60,7 +60,7 @@ class OrganisationResetPasswordTests(base.BaseWebTest):
 
     def test_reset_password(self):
         org = factories.OrganisationWithAccountFactory()
-        self.assertFalse(org.account.password)
+        self.assertIsNone(org.account.password)
         url = self.reverse('management:reset_password', pk=org.pk)
         success_url = self.reverse('management:organisations_view', pk=org.pk)
         resp = self.app.post(url, user='admin')
@@ -68,25 +68,26 @@ class OrganisationResetPasswordTests(base.BaseWebTest):
         resp = resp.follow()
         org = self.assertObjectInDatabase('Organisation', app='bdr_registry',
                                           pk=org.pk)
-        self.assertTrue(org.account.password)
-        msg = [str(m) for m in resp.context['messages']]
-        self.assertTrue(msg)
-        self.assertEqual("Password have been reset. LDAP: {'password': 1}.",
-                         msg[0])
+        self.assertIsNotNone(org.account.password)
+        expected_messages = ["Password have been reset. LDAP: {'password': 1}."]
+        actual_messages = map(str, resp.context['messages'])
+        self.assertItemsEqual(expected_messages, actual_messages)
         self.assertEqual(0, len(mail.outbox))
 
     def test_reset_password_with_perform_send(self):
         obligation = factories.ObligationFactory()
         org = factories.OrganisationWithAccountFactory(obligation=obligation)
         self.assertEqual(1, org.people.count())
-        email = org.people.all()[0].email
+        email = org.people.first().email
         url = self.reverse('management:reset_password', pk=org.pk)
         success_url = self.reverse('management:organisations_view', pk=org.pk)
         resp = self.app.post(url, {'perform_send': '1'}, user='admin')
         self.assertRedirects(resp, success_url)
         resp = resp.follow()
-        msg = [str(m) for m in resp.context['messages']]
-        self.assertTrue(msg)
+        expected_messages = ["Password have been reset. LDAP: {'password': 1}.",
+                             'Emails have been sent to 1 people.']
+        actual_messages = map(str, resp.context['messages'])
+        self.assertItemsEqual(expected_messages, actual_messages)
         self.assertEqual(1, len(mail.outbox))
         self.assertIn(email, mail.outbox[0].to)
 
@@ -120,15 +121,14 @@ class OrganisationCreateAccountTests(base.BaseWebTest):
         resp = self.app.get(url, user='admin', expect_errors=True)
         self.assertEqual(404, resp.status_int)
 
-
     def test_create_organisation_account_get_without_account(self):
         user = factories.BDRGroupUserFactory()
         org = factories.OrganisationFactory()
         url = self.reverse('management:create_account', pk=org.pk)
         resp = self.app.get(url, user=user.username)
         self.assertEqual(200, resp.status_int)
-        self.assertEqual('Create account for %s' % org.name,
-                         resp.pyquery.find('h1').text())
+        self.assertTemplateUsed(resp, 'bdr_management/create_account.html')
+        self.assertEqual(resp.context['object'], org)
 
     def test_create_organisation_account(self):
         user = factories.BDRGroupUserFactory()
@@ -141,13 +141,12 @@ class OrganisationCreateAccountTests(base.BaseWebTest):
         resp = resp.follow()
         org = self.assertObjectInDatabase('Organisation', app='bdr_registry',
                                           pk=org.pk)
-        self.assertTrue(org.account)
-        self.assertTrue(org.account.password)
+        self.assertIsNotNone(org.account)
+        self.assertIsNotNone(org.account.password)
 
-        msg = [str(m) for m in resp.context['messages']]
-        self.assertTrue(msg)
-        self.assertEqual("Account created. LDAP: {'create': 1}.",
-                         msg[0])
+        expected_messages = ["Account created. LDAP: {'create': 1}."]
+        actual_messages = map(str, resp.context['messages'])
+        self.assertItemsEqual(expected_messages, actual_messages)
         self.assertEqual(0, len(mail.outbox))
 
     def test_create_organisation_account_with_perform_send(self):
@@ -156,14 +155,16 @@ class OrganisationCreateAccountTests(base.BaseWebTest):
         person = factories.PersonFactory()
         org = factories.OrganisationFactory(obligation=obligation, people=[person])
         self.assertEqual(1, org.people.count())
-        email = org.people.all()[0].email
+        email = org.people.first().email
         url = self.reverse('management:create_account', pk=org.pk)
         success_url = self.reverse('management:organisations_view', pk=org.pk)
         resp = self.app.post(url, {'perform_send': '1'}, user=user.username)
         self.assertRedirects(resp, success_url)
         resp = resp.follow()
-        msg = [str(m) for m in resp.context['messages']]
-        self.assertTrue(msg)
+        expected_messages = ["Account created. LDAP: {'create': 1}.",
+                             'Emails have been sent to 1 people.']
+        actual_messages = map(str, resp.context['messages'])
+        self.assertItemsEqual(expected_messages, actual_messages)
         self.assertEqual(1, len(mail.outbox))
         self.assertIn(email, mail.outbox[0].to)
 
