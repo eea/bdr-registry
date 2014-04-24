116d3fb7b6d0b726abaf3d37ba9ca94f46847bcc
Ștefan Daniel Mihăilă <stefan.mihaila@gmail.com>
2014-04-24 17:33:55 +0300

    empty_label for Company

--- bdr_management/forms/organisations.py
+++ bdr_management/forms/organisations.py
@@ -1,4 +1,4 @@
-from django.forms import BooleanField, Form, ModelForm
+from django.forms import BooleanField, Form, ModelForm, ModelChoiceField
 from bdr_registry.models import Company
 
 
@@ -7,6 +7,14 @@ class OrganisationForm(ModelForm):
     class Meta():
         model = Company
         exclude = ('id',)
+        empty_label = ""
+
+    def __init__(self, *args, **kwargs):
+        super(OrganisationForm, self).__init__(*args, **kwargs)
+        for field_name in self.fields:
+            field = self.fields[field_name]
+            if isinstance(field, ModelChoiceField):
+                field.empty_label = ''
 
 
 class OrganisationDeleteForm(Form):
