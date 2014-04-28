6c21910a19322fef461b613ebf5c5e775bab826b
Ștefan Daniel Mihăilă <stefan.mihaila@gmail.com>
2014-04-28 17:55:20 +0300

    Add optional ending slash in url patterns

--- bdr_registry/urls.py
+++ bdr_registry/urls.py
@@ -13,26 +13,26 @@ admin.autodiscover()
 company_patterns = patterns(
     '',
 
-    url(r'^add$',
+    url(r'^add/?$',
         views.CompanyCreate.as_view()),
-    url(r'^all$',
+    url(r'^all/?$',
         views.company_all),
 
-    url(r'^(?P<pk>\d+)$',
+    url(r'^(?P<pk>\d+)/?$',
         management_views.CompaniesUpdateView.as_view(),
         name='company'),
-    url(r'^(?P<pk>\d+)/update$',
+    url(r'^(?P<pk>\d+)/update/?$',
         management_views.CompaniesUpdate.as_view(),
         name='company_update'),
 
-    url(r'^(?P<pk>\d+)/persons/add$',
+    url(r'^(?P<pk>\d+)/persons/add/?$',
         management_views.PersonCreate.as_view(),
         name='person_add'),
 
-    url(r'^(?P<pk>\d+)/comment/add',
+    url(r'^(?P<pk>\d+)/comment/add/?$',
         management_views.CommentCreate.as_view(),
         name='comment_add'),
-    url(r'^(?P<pk>\d+)/comment/(?P<comment_pk>\d+)/delete$',
+    url(r'^(?P<pk>\d+)/comment/(?P<comment_pk>\d+)/delete/?$',
         management_views.CommentDelete.as_view(),
         name='comment_delete'),
 )
@@ -40,13 +40,13 @@ company_patterns = patterns(
 person_patterns = patterns(
     '',
 
-    url(r'^(?P<pk>\d+)$',
+    url(r'^(?P<pk>\d+)/?$',
         management_views.PersonView.as_view(),
         name='person'),
-    url(r'^(?P<pk>\d+)/update$',
+    url(r'^(?P<pk>\d+)/update/?$',
         management_views.PersonEdit.as_view(),
         name='person_update'),
-    url(r'^(?P<pk>\d+)/delete$',
+    url(r'^(?P<pk>\d+)/delete/?$',
         management_views.PersonDelete.as_view(),
         name='person_delete'),
 )
@@ -56,18 +56,18 @@ urlpatterns = patterns(
     '',
 
     url(r'^$', views.home, name='home'),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login',
+    url(r'^accounts/login/?$', 'django.contrib.auth.views.login',
         {'template_name': 'login.html'},
         name='login'),
-    url(r'^accounts/logout/$', views.logout_view, name='logout'),
+    url(r'^accounts/logout/?$', views.logout_view, name='logout'),
 
-    url(r'^self_register$', views.SelfRegister.as_view(),
+    url(r'^self_register/?$', views.SelfRegister.as_view(),
         name='self_register'),
-    url(r'^self_register/done$',
+    url(r'^self_register/done/?$',
         TemplateView.as_view(template_name='self_register_done.html'),
         name='self_register_done'),
-    url(r'^crashme$', views.crashme),
-    url(r'^ping$', views.ping),
+    url(r'^crashme/?$', views.crashme),
+    url(r'^ping/?$', views.ping),
 
     url(r'^management/', include('bdr_management.urls',
                                  namespace='management')),
