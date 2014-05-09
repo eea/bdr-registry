ceae48679d8f03e825d620c3977760baa0f89181
Ștefan Daniel Mihăilă <stefan.mihaila@gmail.com>
2014-05-09 13:56:38 +0300

    CSV export persons

--- bdr_management/views/persons.py
+++ bdr_management/views/persons.py
@@ -1,9 +1,12 @@
+from cStringIO import StringIO
+import csv
+
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.core.urlresolvers import reverse, reverse_lazy
 from django.db.models import Q
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext as _
 from django.views import generic
@@ -328,3 +331,27 @@ class PersonCreate(base.CompanyUserRequiredMixin,
         data['breadcrumbs'] = breadcrumbs
         data['cancel_url'] = back_url
         return data
+
+
+class PersonsExport(views.StaffuserRequiredMixin,
+                    generic.View):
+
+    def get(self, request):
+
+        of = StringIO()
+        out = csv.writer(of)
+        out.writerow(['userid', 'companyname', 'country',
+                      'contactname', 'contactemail'])
+        for person in Person.objects.all():
+            org = person.company
+            account = org.account
+            if account is None:
+                continue
+            out.writerow([v.encode('utf-8') for v in [
+                account.uid,
+                org.name,
+                org.country.name,
+                u"{p.title} {p.first_name} {p.family_name}".format(p=person),
+                person.email,
+            ]])
+        return HttpResponse(of.getvalue(), content_type="text/plain")
