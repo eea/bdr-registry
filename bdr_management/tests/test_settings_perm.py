36fced124b12334cb051e963ea5194db397c7dce
Ștefan Daniel Mihăilă <stefan.mihaila@gmail.com>
2014-06-04 11:59:47 +0300

    Add settings permissions test.

---
+++ bdr_management/tests/test_settings_perm.py
@@ -0,0 +1,52 @@
+
+from .base import BaseWebTest
+from bdr_management.tests import factories
+
+
+class SettingsTests(BaseWebTest):
+
+    def test_settings_view_by_anonymous(self):
+        url = self.reverse('management:settings_view')
+        resp = self.app.get(url, expect_errors=True)
+        self.assertEqual(resp.status_int, 403)
+
+    def test_settings_view_by_staff_user(self):
+        user = factories.StaffUserFactory()
+        url = self.reverse('management:settings_view')
+        resp = self.app.get(url, user=user.username)
+        self.assertEqual(200, resp.status_int)
+
+    def test_settings_view_by_bdr_group(self):
+        user = factories.BDRGroupUserFactory()
+        url = self.reverse('management:settings_view')
+        resp = self.app.get(url, user=user.username)
+        self.assertEqual(200, resp.status_int)
+
+    def test_settings_view_by_superuser(self):
+        user = factories.SuperUserFactory()
+        url = self.reverse('management:settings_view')
+        resp = self.app.get(url, user=user.username)
+        self.assertEqual(200, resp.status_int)
+
+    def test_settings_update_by_staff(self):
+        user = factories.StaffUserFactory()
+        url = self.reverse('management:settings_edit')
+        resp = self.app.get(url, user=user.username, expect_errors=True)
+        self.assertEqual(resp.status_int, 403)
+
+    def test_settings_update_by_anonymous(self):
+        url = self.reverse('management:settings_edit')
+        resp = self.app.get(url, expect_errors=True)
+        self.assertEqual(resp.status_int, 403)
+
+    def test_settings_update_by_bdr_group(self):
+        user = factories.BDRGroupUserFactory()
+        url = self.reverse('management:settings_edit')
+        resp = self.app.get(url, user=user.username)
+        self.assertEqual(200, resp.status_int)
+
+    def test_settings_update_by_superuser(self):
+        user = factories.SuperUserFactory()
+        url = self.reverse('management:settings_edit')
+        resp = self.app.get(url, user=user.username)
+        self.assertEqual(200, resp.status_int)
