6e679ecf8f8c99111e1f76eb23b2e19afa705c4e
Ștefan Daniel Mihăilă <stefan.mihaila@gmail.com>
2014-06-10 15:19:47 +0300

    API prototype

--- bdr_registry_api/urls.py
+++ bdr_registry_api/urls.py
@@ -8,6 +8,10 @@ urlpatterns = patterns(
     url(r'^notify/add/file/(?P<account>.*)/?$',
         views.notify_add_file,
         name='notify_add_file'),
-
-
+    url(r'^notify/add/feedback/(?P<account>.*)/?$',
+        views.notify_add_feedback,
+        name='notify_add_feedback'),
+    url(r'^notify/release/(?P<account>.*)/?$',
+        views.notify_release,
+        name='notify_release'),
 )
--- bdr_registry_api/views.py
+++ bdr_registry_api/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User
+from django.db.models import Q
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from post_office.mail import send
 from django.conf import settings
@@ -13,3 +16,27 @@ def notify_add_file(request, *args, **kwargs):
                   if valid_email(p.email)]
 
     send(recipients=recipients, sender=settings.BDR_EMAIL_FROM)
+    return HttpResponse()
+
+
+def notify_add_feedback(request, *args, **kwargs):
+    account = get_object_or_404(Account, uid=kwargs['account'])
+    company = account.company
+    recipients = [p.email for p in company.people.all()
+                  if valid_email(p.email)]
+
+    send(recipients=recipients, sender=settings.BDR_EMAIL_FROM)
+    return HttpResponse()
+
+
+def notify_release(request, *args, **kwargs):
+    account = get_object_or_404(Account, uid=kwargs['account'])
+    company = account.company
+
+    recipients = [u.email for u in User.objects.filter(
+        Q(groups__name=settings.BDR_HELPDESK_GROUP) &
+        Q(obligations__pk=company.obligation.pk))
+        if valid_email(u.email)]
+
+    send(recipients=recipients, sender=settings.BDR_EMAIL_FROM)
+    return HttpResponse()
