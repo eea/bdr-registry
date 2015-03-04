181429b2238f3c60b802855a5bacb2b2d6c6a428
Alex Eftimie <alex@eftimie.ro>
2015-03-04 16:19:13 +0200

    [management] Do not allow editing the obligation for companies already registered.

--- bdr_management/forms/companies.py
+++ bdr_management/forms/companies.py
@@ -4,7 +4,6 @@ from bdr_registry.models import Company
 
 
 class CompanyForm(ModelForm):
-
     class Meta():
         model = Company
         exclude = ('id',)
@@ -13,12 +12,19 @@ class CompanyForm(ModelForm):
         obligations = kwargs.pop('obligations', [])
         super(CompanyForm, self).__init__(*args, **kwargs)
         self.fields['obligation'].queryset = (
-                        self.fields['obligation'].queryset.
-                        filter(pk__in=obligations))
+            self.fields['obligation'].queryset.
+            filter(pk__in=obligations))
+        if self.instance and self.instance.id:
+            self.fields['obligation'].required = False
+            self.fields['obligation'].widget.attrs['disabled'] = 'disabled'
         set_empty_label(self.fields, '')
+
+    def clean_obligation(self):
+        if self.instance and self.instance.id:
+            return self.instance.obligation
+        return self.cleaned_data['obligation']
 
 
 class CompanyDeleteForm(Form):
-
     delete_reporting_folder = BooleanField()
     delete_account = BooleanField()
