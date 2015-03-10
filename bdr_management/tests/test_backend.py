f33134a36c9fc9cab095c9846fe6fd97cc39210f
Alex Eftimie <alex@eftimie.ro>
2015-03-10 17:08:27 +0200

    [testing] fix test_backend tests

--- bdr_management/tests/test_backend.py
+++ bdr_management/tests/test_backend.py
@@ -7,6 +7,7 @@ from bdr_registry.views import send_notification_email
 class BackendTests(base.BaseWebTest):
 
     def test_self_register_mail_to_correct_users(self):
+        factories.SiteConfigurationFactory()
         obligation1 = factories.ObligationFactory()
         obligation2 = factories.ObligationFactory()
         obligation3 = factories.ObligationFactory()
