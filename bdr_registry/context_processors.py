2ceeeb84d49a366c26cd21c3da5077f7f9d0638d
Cornel Nițu <cornel.nitu@eaudeweb.ro>
2016-03-01 15:18:53 +0200

    Refs #69681 hard coded links to BDR

--- bdr_registry/context_processors.py
+++ bdr_registry/context_processors.py
@@ -1,4 +1,8 @@
 from django.conf import settings
 
+
 def settings_context(request):
-    return {'debug': settings.DEBUG}
+    return {
+        'debug': settings.DEBUG,
+        'BDR_SERVER_URL': settings.BDR_SERVER_URL,
+    }
