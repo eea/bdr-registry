a09e542688d1b8eee26a3eb28ba707fc861d60c2
Olimpiu Rob <olimpiu.rob@gmail.com>
2016-03-22 18:25:28 +0200

    Refs #70407; Enhanced zope_api update_organisation_name script to be usable for fgas-cache-server calls as well

--- zope_api/update_organisation_name.py
+++ zope_api/update_organisation_name.py
@@ -1,20 +1,30 @@
 # Python script at /api/update_organisation_name
 request = container.REQUEST
 
+updated = False
+COUNTRY_TO_FOLDER = {
+        'uk': 'gb',
+        'el': 'gr'
+    }
 country_code = request.form['country_code']
+country_folder = COUNTRY_TO_FOLDER.get(country_code, country_code)
 obligation_code = request.form['obligation_folder_name']
 account = request.form['account_uid']
 org_name = request.form['organisation_name']
-updated = False
+if isinstance(org_name, unicode):
+    org_name = org_name.encode('utf-8')
 
+oldcompany_account = request.form.get('oldcompany_account')
+if oldcompany_account:
+    account = oldcompany_account
+
+search_path = str('/'.join([obligation_code, country_folder, account]))
 root = container.restrictedTraverse('/')
-obligation = dict(root.objectItems()).get(obligation_code)
-if obligation:
-    country = dict(obligation.objectItems()).get(country_code)
-    if country:
-        collection = dict(country.objectItems()).get(account)
-        if collection:
-            collection.manage_changeCollection(title=org_name)
-            updated = True
+collection = root.restrictedTraverse(search_path, None)
+
+if collection:
+    if collection.title != org_name:
+        collection.manage_changeCollection(title=org_name)
+        updated = True
 
 return {"updated": updated}
