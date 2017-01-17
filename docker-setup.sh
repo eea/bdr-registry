04f9e1ff39259269ee8496272ee6924cb38a4e80
DragosChirila <dragos.chirila@eaudeweb.ro>
2017-01-17 13:02:22 +0200

    configured gunicorn to dump on stdout and stderr - refs #80762

--- docker-setup.sh
+++ docker-setup.sh
@@ -41,6 +41,8 @@ case $1 in
             --name bdr_registry \
             --bind 0.0.0.0:$BDR_REG_PORT \
             --workers 3
+            --access-logfile -
+            --error-logfile -
         ;;
     *)
 esac
