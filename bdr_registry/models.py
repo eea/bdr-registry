0163da5d59c3b3ffaed11822efa892ed9ba9e786
Diana Boiangiu <boiangiudiana58@gmail.com>
2018-01-03 16:12:08 +0200

    [refs #91009] Fix class representation for python3

--- bdr_registry/models.py
+++ bdr_registry/models.py
@@ -26,7 +26,7 @@ class ApiKey(models.Model):
     key = models.CharField(max_length=255, default=generate_key)
     comment = models.CharField(max_length=255)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.comment
 
 
@@ -38,7 +38,7 @@ class Country(models.Model):
     class Meta(object):
         verbose_name_plural = 'Countries'
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 
@@ -66,7 +66,7 @@ class Obligation(models.Model):
     bcc = models.TextField(blank=True, validators=[validate_comma_separated_email_list])
     admins = models.ManyToManyField(User, related_name='obligations', blank=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
     @transaction.atomic
@@ -104,7 +104,7 @@ class Account(models.Model):
     uid = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255, null=True, blank=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.uid
 
     def set_random_password(self):
@@ -122,7 +122,7 @@ class NextAccountId(models.Model):
     next_id = models.IntegerField()
     obligation = models.OneToOneField(Obligation, null=True, blank=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return u"next_id={p.next_id} ({p.obligation.name})".format(p=self)
 
 
@@ -155,7 +155,7 @@ class Company(models.Model):
     def get_absolute_url(self):
         return reverse('company', kwargs={'pk': self.pk})
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
     def build_reporting_folder_path(self):
@@ -205,7 +205,7 @@ class CompanyNameHistory(models.Model):
     user = models.ForeignKey(User, null=True, blank=True)
     time = models.DateTimeField(auto_now_add=True)
 
-    def __unicode__(self):
+    def __str__(self):
         return u"{h.name} {h.time} {h.user}".format(h=self)
 
 
@@ -231,7 +231,7 @@ class Person(models.Model):
     def formal_name(self):
         return u"{p.title} {p.first_name} {p.family_name}".format(p=self)
 
-    def __unicode__(self):
+    def __str__(self):
         return u"{p.first_name} {p.family_name}".format(p=self)
 
 
@@ -241,7 +241,7 @@ class Comment(models.Model):
     company = models.ForeignKey(Company,
                                 related_name='comments')
 
-    def __unicode__(self):
+    def __str__(self):
         return self.created.strftime('%d %B %Y')
 
 
@@ -252,7 +252,7 @@ class ReportingYear(models.Model):
                                        related_name='reporting_years',
                                        through='ReportingStatus')
 
-    def __unicode__(self):
+    def __str__(self):
         return unicode(self.year)
 
 
@@ -263,7 +263,7 @@ class ReportingStatus(models.Model):
                                        related_name='reporting_statuses')
     reported = models.NullBooleanField(default=None)
 
-    def __unicode__(self):
+    def __str__(self):
         return u"company %s reported in %s: %s" % (
             self.company.pk, self.reporting_year.year, self.reported)
 
