2ed5174a034705ec5b663857041f28ba68ac95c8
Cătălin Jitea <catalinjitea@gmail.com>
2019-07-15 16:15:59 +0300

    [refs #104565] Update docker entrypoint

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
-SSL_CERTS_ROOT='/usr/local/share/ca-certificates'
-
-if [ ! -z "$LDAPS_CACERT" ]; then
-  echo -e "$LDAPS_CACERT" > "$SSL_CERTS_ROOT/CAcustom.crt"
-  dpkg-reconfigure --force ca-certificates
-fi
 
 if [ -z "$POSTGRES_ADDR" ]; then
   export POSTGRES_ADDR="postgres"
@@ -17,19 +11,19 @@ while ! nc -z $POSTGRES_ADDR 5432; do
 done
 
 if ! test -e $BDR_REG_APP/bdr_registry/localsettings.py; then
-    gosu bdrreg python configure.py
+    python configure.py
 fi
 
 args=("$@")
-gosu bdrreg python manage.py collectstatic --noinput
-gosu bdrreg python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py migrate
 
 case $1 in
     manage)
-        gosu bdrreg python manage.py ${args[@]:1}
+        python manage.py ${args[@]:1}
         ;;
     run)
-        gosu bdrreg gunicorn bdr_registry.wsgi:application \
+        gunicorn bdr_registry.wsgi:application \
             --name bdr_registry \
             --bind 0.0.0.0:$BDR_REG_PORT \
             --workers 3 \
